5725aa20505ce75fb0f951f932340a8decf5a19c
David Thorne <datadave333@gmail.com>
2022-04-25 13:43:55 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ def index():
     return render_template('pageS3.html')
 
 
-#can leave this commented out becuase gunicorn is in charge
+
 #if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=443)
 
